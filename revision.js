c106dd267278b784a216611d5aa5fd6cd56e4c98
Omkar Joshi <omkarjoshiop@gmail.com>
2024-01-10 23:25:01 +0530

    Day 1-2

--- revision.js
+++ revision.js
@@ -1,1 +1,15 @@
 console.log("Hey");
+for(let i=1;i<=10;i++)
+{
+    console.log(i);
+}
+
+
+
+function first(message)
+{
+    console.log("From function first");
+    console.log(message);
+}
+
+first('yo');
